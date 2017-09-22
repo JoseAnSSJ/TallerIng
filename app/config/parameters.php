8a414c59e76aaa9ebc929062a56ae2b88bda4f80
JoseAnSSJ <JoseAnSSJ@users.noreply.github.com>
2017-09-22 12:15:26 -0500

    Prestashop

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'PurityWater',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'IAsD9FLRhINKatOvmeuU4FwWknUizxiJvmwNUs7xLvJqOHOqkTOHGsZu',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-09-22',
+    'locale' => 'es-ES',
+    'cookie_key' => 'UINOjFxytiyhK6EAG8v6d6LX7qt2JJ7PWBPc2qzgNeoShhEGfV5EAAa1',
+    'cookie_iv' => 'a5GFEEWL',
+    'new_cookie_key' => 'def00000ff80095b43903502985980701104d4b165447929011c4de94662da87676af971243f682e5f72fc09b1d5d33054f7ade6967ff312b923ecab9486a249b122a539',
+  ),
+);
